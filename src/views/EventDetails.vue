6e12bf6b583e3c72ecc13843d815c496569aef97
damkols <damolausmankolapo@gmail.com>
2021-12-21 22:33:22 -0800

    test run

---
+++ src/views/EventDetails.vue
@@ -0,0 +1,33 @@
+<template>
+  <div v-if="event" class="event">
+    <h1>{{ event.title }}</h1>
+    <p>{{ event.time }} on {{ event.date }} at {{ event.location }}</p>
+    <p>{{ event.description }}</p>
+  </div>
+</template>
+
+<script>
+import EventServices from "@/services/EventServices.js"
+export default {
+    name: "EventDetails",
+    props: ['id'],
+    data() {
+        return {
+            event: null,
+        }
+    },
+    created() {
+        EventServices.getEvent(this.id)
+        .then((response) => {
+            this.event = response.data
+        })
+        .catch((error) => {
+            console.log(error)
+        })
+    },
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/views/EventList.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="home">
+    <EventCard v-for="event in events" :key="event.id" :event="event"/>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import EventCard from '@/components/EventCard.vue';
+import EventServices from '@/services/EventServices.js';
+
+export default {
+  name: 'Home',
+  components: {
+    EventCard
+  },
+  data() {
+    return {
+      events: null,
+    }
+  },
+  created() {
+    EventServices.getEvents()
+      .then((response) => {
+        this.events = response.data
+      })
+      .catch((error) => {
+        console.log(error)
+      })
+  },
+}
+</script>
+
+<style scoped>
+.home{
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+</style>
